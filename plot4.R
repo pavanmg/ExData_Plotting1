b8804c583e43dfecce2a9020a8808124d721759d
Pavan Gajendragad <pavanmg@gmail.com>
2017-04-14 15:40:38 -0700

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -1,28 +1,29 @@
-## Getting full dataset
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-data_full <- read.table("./household_power_consumption.txt", header=T, sep=';', na.strings="?",nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
+voltage <- as.numeric(subSetData$Voltage)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
 
-## Subsetting the data
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_full)
 
-## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
 
-## Plot 4
-par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
-with(data, {
-  plot(Global_active_power~Datetime, type="l", ylab="Global Active Power", xlab="")
-  plot(Voltage~Datetime, type="l", ylab="Voltage", xlab="datetime")
-  plot(Sub_metering_1~Datetime, type="l", ylab="Energy sub metering", xlab="")
-  lines(Sub_metering_2~Datetime,col='Red')
-  lines(Sub_metering_3~Datetime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(Global_reactive_power~Datetime, type="l",ylab="Global_Rective_Power",xlab="datetime")
-})
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
 
-## Saving to file
-dev.copy(png, file="plot4.png", height=480, width=480)
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+
 dev.off()
