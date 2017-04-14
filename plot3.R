13863a969561be85c745219d30fb9aba7590c69f
pavanmg <pavanmg@gmail.com>
2017-04-14 14:28:49 -0700

    axes info corrected

--- plot3.R
+++ plot3.R
@@ -13,11 +13,11 @@ data$Datetime <- as.POSIXct(datetime)
 
 ## Plot 3
 with(data, {
-  plot(Sub_metering_1~Datetime, type="l",ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Sub_metering_1~Datetime, type="l",ylab="Energy sub metering", xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
 })
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2.5,legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## Saving to file
 dev.copy(png, file="plot3.png", height=480, width=480)
